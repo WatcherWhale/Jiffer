e8d6b438bd1531071d8b11abc31ed85c4a50d30f
Mathias Maes <watcherwhale@maes.family>
2021-10-25 13:46:27 +0200

    Remove deployment EPICFAIL

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -15,16 +15,5 @@ module.exports = {
       ],
       NODE_ENV: "development"
     }
-  }],
-  deploy: {
-    production: {
-      "user": "ec2-user",
-      "host": "ec2-3-89-238-173.compute-1.amazonaws.com",
-      "key" : "~/.ssh/jiffer-key.pem",
-      "ref" : "origin/ec2",
-      "repo": "git@github.com:WatcherWhale/ict-arch.git",
-      "path": "/home/ec2-user/jiffer",
-      "post-deploy" : "cd backend && npm install"
-    }
-  }
+  }]
 }
