c265626cf0c4a83f4adea25d16e970fa76160ba3
Mathias Maes <watcherwhale@maes.family>
2021-11-11 15:17:02 +0100

    Added comments

--- lambda/index.js
+++ lambda/index.js
@@ -9,20 +9,12 @@ const {Readable} = require('stream');
 const BIN_PATH = process.env['LAMBDA_TASK_ROOT'] + "/graphicsmagick/bin/";
 const GM = require('gm').subClass({appPath: BIN_PATH});
 
-
-aws.config.update({
-    maxRetries: 2,
-    httpOptions: {
-        timeout: 30000,
-        connectTimeout: 50000
-    }
-});
-
 console.log("Starting lambda...");
 const createGif = async (event, context) => {
     return new Promise(async (resolve, reject) => {
         console.log("Lambda is running.")
 
+        // Get info from which bucket and which file the trigger originated from
         const bucket = event.Records[0].s3.bucket.name;
         const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
@@ -34,12 +26,16 @@ const createGif = async (event, context) => {
 
         try
         {
+            // Download the json file into memory
             const object = await s3.getObject(params).promise();
             const json = JSON.parse(object.Body.toString())
             console.log("Downloading json file")
 
+            // Created a GraphicsMagick process
             let gif = GM();
 
+            // Download files to temporary file system,
+            //   and add pipe them to GraphicsMagick
             for(const i in json.files)
             {
                 const file = json.files[i];
@@ -54,10 +50,13 @@ const createGif = async (event, context) => {
 
             console.log("Downloaded files");
 
+            // Set the GIF paramters
             gif = gif.delay(json.delay);
             gif = gif.quality(json.quality);
 
+            // Write the gif to the temporary filesystem
             gif.write("/tmp/gif.gif", async (err) => {
+
                 if(err)
                 {
                     console.log(err);
@@ -67,14 +66,18 @@ const createGif = async (event, context) => {
 
                 console.log("Created gif")
 
+                // Create the bucket path for the gif
                 const gifKey = path.join(json.key, json.key + ".gif");
 
+                // Upload the gif to the bucket
                 await s3.putObject({
                     Bucket: bucket,
                     Key: gifKey,
                     Body: fs.readFileSync("/tmp/gif.gif")
                 }).promise();
 
+
+                // Remove files from temporary filesystem
                 for(const i in json.files)
                 {
                     fs.rmSync("/tmp/" + i + ".png");
@@ -82,6 +85,7 @@ const createGif = async (event, context) => {
 
                 fs.rmSync("/tmp/gif.gif");
 
+                // Resolve the lambda function, and return the bucket path of the gif
                 resolve(gifKey);
 
                 console.log("Created gif successfully");
@@ -91,17 +95,6 @@ const createGif = async (event, context) => {
         {
             console.log(err);
             reject(err);
-        }
-    });
-}
-
-// https://gist.github.com/wpscholar/270005d42b860b1c33cf5ab25b37928a
-function buffer2Stream(buff)
-{
-    return new Readable({
-        read() {
-            this.push(buff);
-            this.push(null);
         }
     });
 }
