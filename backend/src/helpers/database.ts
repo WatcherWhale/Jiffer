b00616b6b01c0bebadc3fc55021d3c618ce6b266
vanHooijdonkC <cato.van.hooijdonk@hotmail.com>
2021-11-20 00:10:20 +0100

    fixed comments and registering gif

--- backend/src/helpers/database.ts
+++ backend/src/helpers/database.ts
@@ -1,6 +1,7 @@
 import { PutObjectLockConfigurationCommand } from "@aws-sdk/client-s3";
 import {IGif} from "../types/Interfaces";
 import { Config } from "./config";
+//import { Mysql } from "mysql";
 
 
 export class Database
@@ -20,22 +21,36 @@ export class Database
                 console.log("connection with database failed :(")
             console.log("connected")
 
-            connection.query('CREATE DATABASE IF NOT EXISTS JifferDB;')
-            connection.query('USE JifferDB;')
-            connection.query('CREATE TABLE IF NOT EXISTS gifs(uuid CHAR(36) NOT NULL PRIMARY KEY, name VARCHAR(50), path VARCHAR(100), featured TINYINT, creationDate DATE )')
+            connection.query('CREATE DATABASE IF NOT EXISTS JifferDB;', (err: string, results: any)=>{
+                if(err)
+                    console.log("database creation failed")
+                connection.query('USE JifferDB;', (err: any, results: any)=>{
+                    if(err)
+                        console.log("using JifferDB failed")
+                    connection.query('CREATE TABLE IF NOT EXISTS gifs(uuid CHAR(36) NOT NULL PRIMARY KEY, name VARCHAR(50), path VARCHAR(100), featured TINYINT, creationDate DATE )', (err: string, results: any)=>{
+                        if(err)
+                            console.log("table creation failed")
+                        console.log("succesfully created JifferDB and table")
+                    })
+                })
+            })
 
             connection.release()
         })
-
     }
 
-    public async RegisterGif(id: string, path: string) : Promise<void>
+    public async RegisterGif(id: string, name:string, path: string, featured: boolean, creationDate: Date ) : Promise<void>
     {
         this.pool.getConnection((err: string, connection: any)=>{
             if(err)
                 console.log("connection with database failed")
-            console.log("connected database")
-            connection.query()
+            console.log("connected with database")
+            
+            connection.query('INSERT INTO gifs VALUES (?, ?, ?, ?, ?)', { values: [ id, name, path, featured, creationDate ] }, (err: string, result: any)=>{
+                if(err)
+                    console.log("could not insert values in the database")
+            })
+
             connection.release()
         })
     }
@@ -49,6 +64,7 @@ export class Database
             console.log("connected database")
 
             connection.query()
+
             connection.release()
         })
 
