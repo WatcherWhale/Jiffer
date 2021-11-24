b9283e5d05eb976d500005b3afed1de3577e0ffb
Mathias Maes <watcherwhale@maes.family>
2021-11-24 21:12:10 +0100

    Added filenames in file selection box

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -16,12 +16,23 @@ export class FormComponent implements OnInit {
   imageUrl: string = "";
   id: string = "";
   submitted: boolean = false;
+  files :string[] = []
 
   ngOnInit(): void {
 
     setInterval(()=>{
       bulmaSlider.attach()
     },100)
+  }
+
+  filesChanged(files: FileList | null)
+  {
+    if(files == null) return;
+
+    this.files = [];
+
+    for(let i = 0; i < files.length; i++)
+      this.files.push(files[i].name);
   }
 
   submit() {
