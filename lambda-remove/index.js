75411b36ea9131fc5fb39d670fc82f2ac3e8ae63
Bert <bvanitterbeeck@gmail.com>
2021-11-29 14:42:57 +0100

    S3-remove problem, Database works

--- lambda-remove/index.js
+++ lambda-remove/index.js
@@ -6,17 +6,34 @@ const mysql = require('mysql');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 let pool;
-
+console.log("start")
 const handler = async (event) => {
+    console.log("creating connection")
     createPool();
-
+    
     const bucket = process.env['bucket'];
 
     // TODO: CODE HERE
-
+    console.log("query")
     // Look at backend for more an example of how to use this
-    const results = await query("SQL QUERY HERE", []);
+    const results = await query("select * from gifs where creationDate < current_date() and featured = 0", []);
+    const featuredResults = await query("select * from gifs where creationDate + 30 < current_date() and featured = 1",[])
 
+    for (let i in results) {
+        const result = results[i].uuid
+
+        var params = {  Bucket: bucket, Key: result.uuid };
+
+        s3.deleteObject(params, function(err, data) {
+            if (err) console.log(err, err.stack);  // error
+            else     console.log();                 // deleted
+        });
+    }
+
+    await query("delete from gifs where creationDate < current_date() and featured = 0", [])
+    await query("delete from gifs where creationDate + 30 < current_date() and featured = 1",[])
+    console.log(results)
+    console.log(featuredResults)
 }
 
 const createPool = () => {
@@ -47,12 +64,13 @@ const query = (sql, values) => {
     return new Promise(async (resolve, reject) => {
         const connection = await getConnection();
         connection.query(sql, values, (err, results, fields) => {
+            connection.release();
             if(err)
             {
                 reject(err);
                 return;
             }
-
+            console.log(results)
             resolve(results);
         });
     });
